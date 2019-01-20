4e9bb24abf1df1731fb42e8841a2c931475ac985
Aliou Diallo <code@aliou.me>
2019-01-20 18:29:33 +0100

    Create seed ships.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[
+  { name: 'Mercy of Kalr', ship_type: 'supply_carrier' },
+  { name: 'Justice of Toren', ship_type: 'troop_carrier' },
+  { name: 'Sword of Amaat', ship_type: 'war_ship' }
+].each do |attributes|
+  Ship.create!(attributes)
+end

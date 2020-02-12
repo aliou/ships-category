d44ded68ff29c0d2699df9c9c86251942c7cc5bb
Aliou Diallo <code@aliou.me>
2020-02-12 09:27:05 +0100

    Use our custom attribute type for the category.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,10 +1,6 @@
 class Ship < ApplicationRecord
-  VALID_CATEGORIES = %w(
-    shuttle supply_carrier troop_carrier war_ship
-  ).freeze
+  attribute :category, :ship_category
 
   validates :name, presence: true
   validates :category, presence: true
-
-  validates :category, inclusion: { in: VALID_CATEGORIES }
 end
